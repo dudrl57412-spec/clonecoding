13532a6c311aa9d0aa59bc741be045430765c801
dudrl57412-spec <dudrl57412@gmail.com>
2026-04-09 17:31:52 +0900

    클론코딩 완성본

--- js/mian.js
+++ js/mian.js
@@ -18,3 +18,33 @@ const swiper = new Swiper(".visual-sw", {
 
   speed: 1500,
 });
+// gotop 스크롤 이벤트
+// 변수 선언
+const goTop = document.querySelector("#gotop");
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 700) {
+    goTop.style.display = "block";
+  } else {
+    goTop.style.display = "none";
+  }
+});
+//   gotop버튼 클릭시 위로 스크롤
+goTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+// 헤더 색상변경
+const headerbg = document.querySelector(".header");
+// const topbanner = document.querySelector(".top_banner");
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 0) {
+    headerbg.style.background = "rgba(0,0,0,0.5)";
+    headerbg.style.top = "0";
+    // topbanner.style.display ="none"
+  } else {
+    headerbg.style.background = "transparent";
+    headerbg.style.top = "30px";
+  }
+});
